20281cd721ebbcda74fb6a41f7854b583abf7a4e
ghostjogger <19Sqoevvgs78>
2018-04-08 12:23:02 +0100

    new

--- Scenes/GameScene.swift
+++ Scenes/GameScene.swift
@@ -19,6 +19,37 @@ enum GameState {
 class GameScene: SKScene {
     
 
+    let startLabel = SKLabelNode(text: "Start")
+    
+    
+    override func didMove(to view: SKView) {
+        
+        startLabel.fontName = "Jellee-Roman"
+        startLabel.fontColor = UIColor.white
+        startLabel.fontSize = 100
+        startLabel.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.1)
+        self.addChild(startLabel)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch: AnyObject in touches{
+            
+            let pointOfTouch = touch.location(in: self)
+            
+            if startLabel.contains(pointOfTouch){
+                
+                let sceneToMoveTo = MainMenuScene(size: self.size)
+                sceneToMoveTo.scaleMode = self.scaleMode
+                let myTransition = SKTransition.fade(withDuration: 0.5)
+                self.view!.presentScene(sceneToMoveTo, transition:myTransition)
+            }
+            
+        }
+        
+    }
+ 
+    
 }
     
     
