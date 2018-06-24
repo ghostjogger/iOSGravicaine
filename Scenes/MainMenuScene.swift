e64045197a43258b71a6b23564d44f1a2c282451
ghostjogger <19Sqoevvgs78>
2018-06-24 16:10:53 +0100

    better

--- Scenes/MainMenuScene.swift
+++ Scenes/MainMenuScene.swift
@@ -24,17 +24,33 @@ class MainMenuScene: SKScene{
     var titleScaleFactor = 1.0
     var scale:CGFloat = 1.0
 
-    //credit panel setup
+    //score panel setup
     var scoreattributes: EKAttributes
     var scoremessage: EKSimpleMessage
     var scorenotificationMessage: EKNotificationMessage
     var scorenotificationView: EKNotificationMessageView
+    
+    //credit panel setup
+    var creditattributes: EKAttributes
+    var creditmessage: EKSimpleMessage
+    var creditnotificationMessage: EKNotificationMessage
+    var creditnotificationView: EKNotificationMessageView
 
     //star animation variables
     
     private var protonStarFrames: [SKTexture] = []
     let protonStarAnimatedAtlas = SKTextureAtlas(named: "star")
     var protonFrames: [SKTexture] = []
+    
+    var score:Int
+    var highScoreName: String?
+    
+    var musicLabel = SKLabelNode(text: "Game Music")
+    var transLabel = SKLabelNode(text: "Grav Transition Marker")
+    var music: Bool
+    var trans: Bool
+    var musicNode: SKSpriteNode = SKSpriteNode()
+    var transNode: SKSpriteNode = SKSpriteNode()
     
     override init(size:CGSize) {
         
@@ -54,22 +70,48 @@ class MainMenuScene: SKScene{
             UserDefaults.standard.set("", forKey: HighScoreName)
         }
 
+        score = UserDefaults.standard.integer(forKey: HighScoreKey)
+        highScoreName = UserDefaults.standard.string(forKey: HighScoreName)
+        music = UserDefaults.standard.bool(forKey: "music")
+        trans = UserDefaults.standard.bool(forKey: "trans")
         
-        // credits pop-up
+        musicLabel.fontSize = 30.0 * scale
+        musicLabel.fontName = FontName
+        
+        transLabel.fontSize = 30.0 * scale
+        transLabel.fontName = FontName
+        
+        // highest score pop-up
         scoreattributes = EKAttributes.centerFloat
-        scoreattributes.displayDuration = .infinity
+        scoreattributes.displayDuration = 3.0
         scoreattributes.entryInteraction = .dismiss
         scoreattributes.entryBackground = .image(image: UIImage(named: "panelBackground")!)
         scoreattributes.popBehavior = .animated(animation: .init(translate: .init(duration: 0.3), scale: .init(from: 1, to: 0.7, duration: 0.7)))
         scoreattributes.shadow = .active(with: .init(color: .black, opacity: 0.5, radius: 10, offset: .zero))
         scoremessage = EKSimpleMessage(image: EKProperty.ImageContent(imageName: "gravicaineIcon32"),
-                                       title: EKProperty.LabelContent(text: "Credits:",
+                                       title: EKProperty.LabelContent(text: "Highest Score:",
                                                                       style: EKProperty.Label(font: UIFont(name: FontName, size: 20.0)!, color: UIColor.white, alignment: NSTextAlignment.center)),
-                                       description: EKProperty.LabelContent(text: "Stephen \n\n  Ball \n\n Is \n\n The Greatest",
+                                       description: EKProperty.LabelContent(text: "\n \(highScoreName!) \n \n  \(score)",
                                         style: EKProperty.Label(font: UIFont(name: FontName, size: 15.0)!, color: UIColor.white, alignment: NSTextAlignment.center)))
         
         scorenotificationMessage = EKNotificationMessage(simpleMessage: scoremessage)
         scorenotificationView = EKNotificationMessageView(with: scorenotificationMessage)
+        
+        // credits pop-up
+        creditattributes = EKAttributes.centerFloat
+        creditattributes.displayDuration = 5.0
+        creditattributes.entryInteraction = .dismiss
+        creditattributes.entryBackground = .image(image: UIImage(named: "panelBackground")!)
+        creditattributes.popBehavior = .animated(animation: .init(translate: .init(duration: 0.3), scale: .init(from: 1, to: 0.7, duration: 0.7)))
+        creditattributes.shadow = .active(with: .init(color: .black, opacity: 0.5, radius: 10, offset: .zero))
+        creditmessage = EKSimpleMessage(image: EKProperty.ImageContent(imageName: "gravicaineIcon32"),
+                                       title: EKProperty.LabelContent(text: "Credits:",
+                                                                      style: EKProperty.Label(font: UIFont(name: FontName, size: 20.0)!, color: UIColor.white, alignment: NSTextAlignment.center)),
+                                       description: EKProperty.LabelContent(text: "\n Stephen Ball \n \n  is \n brilliant!!!",
+                                        style: EKProperty.Label(font: UIFont(name: FontName, size: 15.0)!, color: UIColor.white, alignment: NSTextAlignment.center)))
+        
+        creditnotificationMessage = EKNotificationMessage(simpleMessage: creditmessage)
+        creditnotificationView = EKNotificationMessageView(with: creditnotificationMessage)
 
         super.init(size: size)
      
@@ -121,25 +163,59 @@ class MainMenuScene: SKScene{
         
         let laser = GreenShieldNode(scale: self.scale)
         laser.zPosition = 100
-        laser.position = CGPoint(x: self.frame.width * 0.6, y: self.frame.height * 0.2)
+        laser.position = CGPoint(x: self.frame.width * 0.5, y: self.frame.height * 0.2)
         self.addChild(laser)
         laser.animate()
         
-        self.startLabel.position = CGPoint(x: self.frame.width * 0.6, y: self.frame.height * 0.2)
+        self.startLabel.position = CGPoint(x: self.frame.width * 0.5, y: self.frame.height * 0.2)
         self.startLabel.size = CGSize(width: self.startLabel.size.width * self.scale, height: self.startLabel.size.height * self.scale )
         self.addChild(self.startLabel)
         
-        self.optionLabel.position = CGPoint(x: self.frame.width * 0.15, y: self.frame.height * 0.30)
-        self.optionLabel.size = CGSize(width: self.optionLabel.size.width * self.scale, height: self.optionLabel.size.height * self.scale )
-        self.addChild(self.optionLabel)
-        
+
         self.creditLabel.position = CGPoint(x: self.frame.width * 0.15, y: self.frame.height * 0.20)
         self.creditLabel.size = CGSize(width: self.creditLabel.size.width * self.scale, height: self.creditLabel.size.height * self.scale )
         self.addChild(self.creditLabel)
         
-        self.scoresLabel.position = CGPoint(x: self.frame.width * 0.15, y: self.frame.height * 0.10)
+        self.scoresLabel.position = CGPoint(x: self.frame.width * 0.85, y: self.frame.height * 0.20)
         self.scoresLabel.size = CGSize(width: self.scoresLabel.size.width * self.scale, height: self.scoresLabel.size.height * self.scale )
         self.addChild(self.scoresLabel)
+        
+        musicLabel.position = CGPoint(x: self.size.width / 2, y: self.size.height * 0.8)
+        self.addChild(musicLabel)
+        
+        transLabel.position = CGPoint(x: self.size.width / 2, y: self.size.height * 0.6)
+        self.addChild(transLabel)
+        
+        if music{
+            musicNode = SKSpriteNode(imageNamed: "on")
+            musicNode.size = CGSize(width: musicNode.size.width * scale,
+                                    height: musicNode.size.height * scale)
+        }
+        else{
+            musicNode = SKSpriteNode(imageNamed: "off")
+            musicNode.size = CGSize(width: musicNode.size.width * scale,
+                                    height: musicNode.size.height * scale)
+        }
+        
+        if trans{
+            transNode = SKSpriteNode(imageNamed: "on")
+            transNode.size = CGSize(width: transNode.size.width * scale,
+                                    height: transNode.size.height * scale)
+        }
+        else{
+            transNode = SKSpriteNode(imageNamed: "off")
+            transNode.size = CGSize(width: transNode.size.width * scale,
+                                    height: transNode.size.height * scale)
+        }
+        
+        musicNode.zPosition = 200
+        musicNode.position = CGPoint(x: self.frame.width/2, y: self.frame.height * 0.75)
+        self.addChild(musicNode)
+        
+        transNode.zPosition = 200
+        transNode.position = CGPoint(x: self.frame.width/2, y: self.frame.height * 0.55)
+        self.addChild(transNode)
+        
         
         self.addChild(menuBackgroundSound)
         menuBackgroundSound.run(SKAction.changeVolume(to: 5, duration: 0.1))
@@ -169,41 +245,30 @@ class MainMenuScene: SKScene{
                 }
             }
             
-            if optionLabel.contains(pointOfTouch){
-                
-                let sceneToMoveTo = OptionScene(size: self.size)
-                sceneToMoveTo.scaleMode = self.scaleMode
-                let myTransition = SKTransition.moveIn(with: SKTransitionDirection.left, duration: 0.5)
-                SwiftEntryKit.dismiss()
-                let action = SKAction.changeVolume(to: 0, duration: 1.0)
-                let removeAction = SKAction.removeFromParent()
-                let sequence = SKAction.sequence([action,removeAction])
-                menuBackgroundSound.run(sequence) {
-                    self.view!.presentScene(sceneToMoveTo, transition:myTransition)
-                }
-  
-            }
+
             
             if scoresLabel.contains(pointOfTouch){
                 
-                let sceneToMoveTo = ScoreScene(size: self.size)
-                sceneToMoveTo.scaleMode = self.scaleMode
-                let myTransition = SKTransition.moveIn(with: SKTransitionDirection.right, duration: 0.5)
-                SwiftEntryKit.dismiss()
-                let action = SKAction.changeVolume(to: 0, duration: 1.0)
-                let removeAction = SKAction.removeFromParent()
-                let sequence = SKAction.sequence([action,removeAction])
-                menuBackgroundSound.run(sequence) {
-                    self.view!.presentScene(sceneToMoveTo, transition:myTransition)
+                if score > 0{
+                    SwiftEntryKit.dismiss()
+                    SwiftEntryKit.display(entry: scorenotificationView, using: scoreattributes)
                 }
-                
                 
             }
             
             if creditLabel.contains(pointOfTouch){
                 
-                SwiftEntryKit.display(entry: scorenotificationView, using: scoreattributes)
-                
+                SwiftEntryKit.dismiss()
+                SwiftEntryKit.display(entry: creditnotificationView, using: creditattributes)
+
+            }
+            
+            if musicNode.contains(pointOfTouch){
+                toggleMusic()
+            }
+            
+            if transNode.contains(pointOfTouch){
+                toggleTrans()
             }
             
         }
@@ -224,6 +289,67 @@ class MainMenuScene: SKScene{
         node.run(sequence)
         
     }
+    
+    func toggleMusic(){
+        
+        music = !music
+        UserDefaults.standard.set(music, forKey: "music")
+        
+        if music{
+            
+            musicNode.removeFromParent()
+            musicNode = SKSpriteNode(imageNamed: "on")
+            musicNode.zPosition = 200
+            musicNode.size = CGSize(width: musicNode.size.width * scale,
+                                    height: musicNode.size.height * scale)
+            musicNode.position = CGPoint(x: self.frame.width/2, y: self.frame.height * 0.75)
+            self.addChild(musicNode)
+            
+        }
+        else{
+            musicNode.removeFromParent()
+            musicNode = SKSpriteNode(imageNamed: "off")
+            musicNode.zPosition = 200
+            musicNode.size = CGSize(width: musicNode.size.width * scale,
+                                    height: musicNode.size.height * scale)
+            musicNode.position = CGPoint(x: self.frame.width/2, y: self.frame.height * 0.75)
+            self.addChild(musicNode)
+            
+        }
+        
+        
+    }
+    
+    func toggleTrans(){
+        
+        trans = !trans
+        UserDefaults.standard.set(trans, forKey: "trans")
+        
+        if trans{
+            
+            transNode.removeFromParent()
+            transNode = SKSpriteNode(imageNamed: "on")
+            transNode.zPosition = 200
+            transNode.size = CGSize(width: transNode.size.width * scale,
+                                    height: transNode.size.height * scale)
+            transNode.position = CGPoint(x: self.frame.width/2, y: self.frame.height * 0.55)
+            self.addChild(transNode)
+            
+        }
+        else{
+            transNode.removeFromParent()
+            transNode = SKSpriteNode(imageNamed: "off")
+            transNode.zPosition = 200
+            transNode.size = CGSize(width: transNode.size.width * scale,
+                                    height: transNode.size.height * scale)
+            transNode.position = CGPoint(x: self.frame.width/2, y: self.frame.height * 0.55)
+            self.addChild(transNode)
+            
+        }
+        
+        
+    }
+    
     
     
 }
