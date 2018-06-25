14f12718f7ef78d016e918518812cc6f41884d14
ghostjogger <19Sqoevvgs78>
2018-06-25 12:12:38 +0100

    tweaked

--- Scenes/GameWonScene.swift
+++ Scenes/GameWonScene.swift
@@ -100,6 +100,107 @@ class GameWonScene: SKScene {
         titleImage.zPosition = 10
         titleImage.setScale(CGFloat(titleScaleFactor))
         addChild(titleImage)
+        
+
+
+        cNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        cNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(cNode)
+        oNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        oNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(oNode)
+        nNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        nNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(nNode)
+        gNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        gNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(gNode)
+        rNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        rNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(rNode)
+        aNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        aNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(aNode)
+        tNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        tNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(tNode)
+        uNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        uNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(uNode)
+        lNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        lNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(lNode)
+        aNode2.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        aNode2.setScale(CGFloat(titleScaleFactor))
+        self.addChild(aNode2)
+        tNode2.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        tNode2.setScale(CGFloat(titleScaleFactor))
+        self.addChild(tNode2)
+        iNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        iNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(iNode)
+        oNode2.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        oNode2.setScale(CGFloat(titleScaleFactor))
+        self.addChild(oNode2)
+        nNode2.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        nNode2.setScale(CGFloat(titleScaleFactor))
+        self.addChild(nNode2)
+        sNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        sNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(sNode)
+        excNode.position = CGPoint(x: random(min: self.frame.minX + 20, max: self.frame.maxX - 20), y: self.frame.minY - 100)
+        excNode.setScale(CGFloat(titleScaleFactor))
+        self.addChild(excNode)
+        
+        
+        let waitAction = SKAction.wait(forDuration: 0.5)
+        let thudSound = SKAction.playSoundFileNamed("sound62.wav", waitForCompletion: false)
+        let moveSpeed: TimeInterval = 1.0
+        let cHeight: CGFloat = 0.75
+        
+        
+        let move1 = SKAction.move(to: CGPoint(x: self.frame.width * 0.1, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move2 = SKAction.move(to: CGPoint(x: self.frame.width * 0.153, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move3 = SKAction.move(to: CGPoint(x: self.frame.width * 0.206, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move4 = SKAction.move(to: CGPoint(x: self.frame.width * 0.259, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move5 = SKAction.move(to: CGPoint(x: self.frame.width * 0.312, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move6 = SKAction.move(to: CGPoint(x: self.frame.width * 0.365, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move7 = SKAction.move(to: CGPoint(x: self.frame.width * 0.418, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move8 = SKAction.move(to: CGPoint(x: self.frame.width * 0.471, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move9 = SKAction.move(to: CGPoint(x: self.frame.width * 0.524, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move10 = SKAction.move(to: CGPoint(x: self.frame.width * 0.577, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move11 = SKAction.move(to: CGPoint(x: self.frame.width * 0.63, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move12 = SKAction.move(to: CGPoint(x: self.frame.width * 0.683, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move13 = SKAction.move(to: CGPoint(x: self.frame.width * 0.736, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move14 = SKAction.move(to: CGPoint(x: self.frame.width * 0.789, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move15 = SKAction.move(to: CGPoint(x: self.frame.width * 0.842, y: self.frame.height * cHeight), duration: moveSpeed)
+        let move16 = SKAction.move(to: CGPoint(x: self.frame.width * 0.9, y: self.frame.height * cHeight), duration: moveSpeed)
+        
+        cNode.run(SKAction.sequence([waitAction,move1,thudSound]))
+        oNode.run(SKAction.sequence([waitAction,move2,thudSound]))
+        nNode.run(SKAction.sequence([waitAction,move3,thudSound]))
+        gNode.run(SKAction.sequence([waitAction,move4,thudSound]))
+        rNode.run(SKAction.sequence([waitAction,move5,thudSound]))
+        aNode.run(SKAction.sequence([waitAction,move6,thudSound]))
+        tNode.run(SKAction.sequence([waitAction,move7,thudSound]))
+        uNode.run(SKAction.sequence([waitAction,move8,thudSound]))
+        lNode.run(SKAction.sequence([waitAction,move9,thudSound]))
+        aNode2.run(SKAction.sequence([waitAction,move10,thudSound]))
+        tNode2.run(SKAction.sequence([waitAction,move11,thudSound]))
+        iNode.run(SKAction.sequence([waitAction,move12,thudSound]))
+        oNode2.run(SKAction.sequence([waitAction,move13,thudSound]))
+        nNode2.run(SKAction.sequence([waitAction,move14,thudSound]))
+        sNode.run(SKAction.sequence([waitAction,move15,thudSound]))
+        excNode.run(SKAction.sequence([waitAction,move16,thudSound]))
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
     func spawnAnimation(){
