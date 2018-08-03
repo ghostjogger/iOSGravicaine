9855f2657e3ccf9ea51bdbe4f76e728aa0d68572
ghostjogger <19Sqoevvgs78>
2018-08-03 17:51:11 +0100

    yeah

--- Scenes/GameScene.swift
+++ Scenes/GameScene.swift
@@ -450,7 +450,7 @@ class GameScene: SKScene, GameLogicDelegate, UITextFieldDelegate {
         
         
  
-        barrierCount = 80
+        barrierCount = 130
         
         print(barrierTypes.count)
         print(barriers.count)
